e399c9aa1cb28e675fb160c9302fe426c34e30f3
finvo-invoice <finvo.invoice@gmail.com>
2025-05-02 15:51:09 +0530

    Add files via upload

---
+++ frontend/build/static/js/388.f638b52e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[388],{9388:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=388.f638b52e.chunk.js.map
